0819aa20783be213ad6c80db742ab7aebb00327a
noctua <noctuarox@seznam.cz>
2017-02-09 16:07:19 +0100

    feat(autocomplete): basic implementation of control through arrowkeys and tabkey

--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Renderer, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-autocomplete',
   templateUrl: './autocomplete.component.html',
   styleUrls: ['./autocomplete.component.scss']
 })
-export class AutocompleteComponent {
+export class AutocompleteComponent implements OnInit {
 
   @Input() keywords = [];
   inputText: string = "";
@@ -14,7 +14,9 @@ export class AutocompleteComponent {
 
   onInputChange(event: any) {
     if (this.inputText.length >= 3) {
-      this.findMatch();
+      this.matchingText = this.keywords.filter((el) =>
+        el.toLowerCase().indexOf(this.inputText.toLowerCase()) > -1
+      );
       this.searchingForMatch = true;
     }
     else {
@@ -22,19 +24,34 @@ export class AutocompleteComponent {
     }
   }
 
-  findMatch() {
-    this.matchingText = this.filterText(this.inputText);
+  selectTextToAutocomplete(event: any, item: string) {
+    if (event.type === 'click' || event.key === 'Enter') {
+      this.inputText = item;
+      this.searchingForMatch = false;
+    }
   }
 
-  filterText(query) {
-    return this.keywords.filter((el) =>
-      el.toLowerCase().indexOf(query.toLowerCase()) > -1
-    )
+  constructor(private renderer: Renderer) {
+
   }
 
-  selectTextToAutocomplete(index){
-    this.inputText = this.matchingText[index];
-    this.matchingText = [];
+  ngOnInit() {
+    document.addEventListener('keydown', (event) => {
+      let tabIndex = (<HTMLElement>document.activeElement).tabIndex;
+      if (event.key === 'ArrowUp') {
+        tabIndex--;
+      }
+      if (event.key === 'ArrowDown') {
+        console.log("arrowdown");
+        tabIndex++;
+      }
+      const elementToFocus = document.querySelector(`[tabindex="${tabIndex}"]`);
+      console.log(`tabindex="${tabIndex}"`);
+      console.log({tabIndex});
+      this.renderer.invokeElementMethod(elementToFocus, 'focus', []);
+    }, false);
   }
+
 
 }
+
